6b295d9a72e76f2a8094bc3121557ef180413609
june29 <june29.jp@gmail.com>
2020-06-23 14:07:44 +0900

    404 Nikki Not Found

--- app/controllers/nikki_controller.rb
+++ app/controllers/nikki_controller.rb
@@ -1,14 +1,25 @@
 require "open-uri"
 
 class NikkiController < ApplicationController
+  class NikkiNotFound < StandardError; end
+
+  rescue_from OpenURI::HTTPError, with: :render_404
+  rescue_from NikkiNotFound, with: :render_404
+
   def show
     date = params[:date]
     data = JSON.parse(URI.open("https://scrapbox.io/api/pages/june29/#{date}").read)
     nikki = data.dig("relatedPages", "links1hop").find { |page|
       page.dig("title").starts_with?(Regexp.compile("#{date} \\w{3} : "))
     }
-    page_name = CGI.escape(nikki.dig("title").gsub(" ", "_"))
 
+    raise NikkiNotFound if nikki.nil?
+
+    page_name = CGI.escape(nikki.dig("title").gsub(" ", "_"))
     redirect_to "https://scrapbox.io/june29/#{page_name}"
+  end
+
+  def render_404
+    render(plain: "404 Nikki Not Found")
   end
 end
