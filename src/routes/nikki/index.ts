39c4bc7ef50a101598b5cf3c140c7a3bd99acc73
june29 <june29.jp@gmail.com>
2022-04-29 22:03:22 +0900

    Nikkis

--- src/routes/nikki/index.ts
+++ src/routes/nikki/index.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
 
 router.get("/", (request: express.Request, response: express.Response) => {
-  response.status(200).json({ message: "nikki" });
+  const url = "https://scrapbox.io/api/pages/june29/" + encodeURIComponent("日記");
+  const nikkiRegex = /^\d{4}-\d{2}-\d{2} \w{3} : /
+
+  axios.get(url).then((scrapbox) => {
+    const nikkis = scrapbox.data["relatedPages"]["links1hop"].filter((page: any) => {
+      return page.title.match(nikkiRegex);
+    }).sort((a: any, b: any) => {
+      return b.title.localeCompare(a.title);
+    });
+    response.render("nikki/index", { layout: "layout", title: "Nikki", nikkis: nikkis });
+  }).catch((error) => {
+    console.log(error);
+  });
+
 });
 
 router.get("/:date", (request: express.Request, response: express.Response) => {
