aebe89e4ac93ef915273063b82ef847df3fafd93
june29 <june29.jp@gmail.com>
2022-04-29 22:42:35 +0900

    Organize

--- src/routes/nikki/index.ts
+++ src/routes/nikki/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import axios from "axios";
 
 const router = express.Router();
+const nikkiRegex = /^\d{4}-\d{2}-\d{2} \w{3} : /;
+const scrapboxApiBaseUrl = "https://scrapbox.io/api/pages/june29/";
 
 router.get("/", (request: express.Request, response: express.Response) => {
-  const url = "https://scrapbox.io/api/pages/june29/" + encodeURIComponent("日記");
-  const nikkiRegex = /^\d{4}-\d{2}-\d{2} \w{3} : /
+  const url = scrapboxApiBaseUrl + encodeURIComponent("日記");
 
   axios.get(url).then((scrapbox) => {
     const nikkis = scrapbox.data["relatedPages"]["links1hop"].filter((page: any) => {
@@ -13,6 +14,7 @@ router.get("/", (request: express.Request, response: express.Response) => {
     }).sort((a: any, b: any) => {
       return b.title.localeCompare(a.title);
     });
+
     response.render("nikki/index", { layout: "layout", title: "Nikki", nikkis: nikkis });
   }).catch((error) => {
     console.log(error);
@@ -21,12 +23,10 @@ router.get("/", (request: express.Request, response: express.Response) => {
 
 router.get("/:date", (request: express.Request, response: express.Response) => {
   const date = request.params["date"];
-  const url = "https://scrapbox.io/api/pages/june29/" + date;
-  const nikkiRegex = /^\d{4}-\d{2}-\d{2} \w{3} : /
+  const url = scrapboxApiBaseUrl + date;
 
   axios.get(url).then((scrapbox) => {
     const nikki = scrapbox.data["relatedPages"]["links1hop"].filter((page: any) => {
-      console.log(page);
       return page.title.match(nikkiRegex);
     })[0];
     const page_name = encodeURIComponent(nikki.title.replace(" ", "_"));
