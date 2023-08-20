704c5c73c7c883e457fd4310525d5ddf7be4f0d8
june29 <june29.jp@gmail.com>
2023-08-20 23:42:17 +0900

    別で組み立てたフィードを返す形にする

--- src/routes/nikki/index.ts
+++ src/routes/nikki/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import axios from "axios";
-import { Feed } from "feed";
 
 const router = express.Router();
 const nikkiRegex = /^\d{4}-\d{2}-\d{2} \w{3} : /;
@@ -23,59 +22,8 @@ router.get("/", (request: express.Request, response: express.Response) => {
 });
 
 router.get("/atom.xml", (request: express.Request, response: express.Response) => {
-  const url = "https://scrapbox.io/api/pages/june29?sort=created&limit=30";
-
-  axios.get(url).then((scrapbox) => {
-    const nikkis = scrapbox.data.pages.filter((page: any) => {
-      return page.title.match(nikkiRegex) && !page.title.includes("(書きかけ)");
-    }).sort((a: any, b: any) => {
-      return b.title.localeCompare(a.title);
-    }).slice(0, 10);
-
-    const latestNikki = nikkis[0];
-    const latestNikkiDate = new Date(latestNikki.updated * 1000);
-
-    const feed = new Feed({
-      title: "純朴日記",
-      description: "junebokuが書いている日記です",
-      id: "https://scrapbox.io/june29",
-      link: "https://scrapbox.io/june29",
-      language: "ja",
-      image: "https://june29.jp/img/ring.jpg",
-      favicon: "https://scrapbox.io/assets/img/favicon/favicon.ico",
-      copyright: "All rights reserved " + latestNikkiDate.getFullYear() + ", juneboku",
-      updated: latestNikkiDate,
-      generator: "https://api.june29.jp/",
-      feedLinks: {
-        atom: "https://api.june29.jp/nikki/atom.xml"
-      },
-      author: {
-        name: "Jun OHWADA / juneboku",
-        email: "juneboku@gmail.com",
-        link: "https://juneboku.xyz"
-      }
-    });
-
-    nikkis.forEach((nikki: any) => {
-      const link = `https://scrapbox.io/june29/${encodeURIComponent(nikki.title)}`;
-      const description = nikki.descriptions.filter((line: string) => {
-        return !(line.startsWith("[***") || line.startsWith("[https://gyazo.com/"));
-      }).map((line: string) => {
-        return "<p>" + line.replace(/(\[|\])/g, "") + "</p>";
-      }).join("\n");
-      const image = `<p><img src="${nikki.image}"></p>`;
-
-      feed.addItem({
-        title: nikki.title,
-        description: `${description}${image}`,
-        id: link,
-        link: link,
-        content: "",
-        date: new Date(nikki.updated * 1000)
-      });
-    });
-
-    response.send(feed.atom1());
+  axios.get("https://june29.github.io/nikki-feed/feed.xml").then((feed) => {
+    response.send(feed.data);
   }).catch((error) => {
     console.log(error);
   });
